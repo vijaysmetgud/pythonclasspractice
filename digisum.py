4abc1f3b7bbfb6351646b74c56e3ced95566ca95
vijay <vsmetgud@gmail.com>
2024-04-20 23:20:07 +0530

    added python examples

---
+++ digisum.py
@@ -0,0 +1,21 @@
+number = 1000
+q = 0
+sum = 0
+remainder = 0
+while number >= 10: 
+   remainder = number % 10
+   sum = remainder + sum
+   q = number // 10
+   if q < 10:    
+      sum = sum + q 
+      break
+   else:
+      number = q   
+if sum >= 10:
+   z = 0
+   y = 0
+   y = sum % 10
+   z = sum // 10
+   sum = y + z   
+print(sum) 
+    
